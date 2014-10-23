8e24afa53a0591572f2c266d02f1b03dc84c81ac
Ian Foote <ian@incuna.com>
2014-10-23 12:22:55 +0100

    Add basic setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+install_requires = (
+)
+
+
+setup(
+    name='django-translatable-fields',
+    packages=find_packages(),
+    include_package_data=True,
+    version='0.0.0',
+    description='',
+    author='Incuna',
+    author_email='admin@incuna.com',
+    url='',
+    install_requires=install_requires,
+    zip_safe=False,
+)
