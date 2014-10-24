ff9d47819b7ec5b87775fa44a47b902bebe6fada
Ian Foote <ian@incuna.com>
2014-10-24 14:57:38 +0100

    Add validate_languages validator
    * Restrict TranslatableField keys to langages in settings.LANGUAGES

--- tests/test_fields/test_fields.py
+++ tests/test_fields/test_fields.py
@@ -1,5 +1,7 @@
-from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.test import override_settings, TestCase
 
+from translatable_fields import fields
 from .models import TranslatableModel
 
 
@@ -12,3 +14,29 @@ class TestTranslatableField(TestCase):
     def test_save(self):
         instance = TranslatableModel()
         instance.save()
+
+    @override_settings(LANGUAGES=(('en', 'English'), ('fr', 'French')))
+    def test_allow_supported_languages(self):
+        """Store translations for supported languages."""
+        instance = TranslatableModel(name={'en': 'Name'})
+        instance.clean_fields()
+
+    @override_settings(LANGUAGES=(('de', 'German'), ('fr', 'French')))
+    def test_disallow_unsupported_languages(self):
+        """Don't Store translations for unsupported languages."""
+        instance = TranslatableModel(name={'en': 'Name'})
+        with self.assertRaises(ValidationError):
+            instance.clean_fields()
+
+
+class TestLanguageTemplate(TestCase):
+    """Use the appropriate error template when validating languages."""
+    def test_one_language(self):
+        """Use the appropriate error template for one language."""
+        template = fields.language_template(1)
+        self.assertEqual(template, fields.SINGLE_LANGUAGE_TEMPLATE)
+
+    def test_multiple_languages(self):
+        """Use the appropriate error template for multiple languages."""
+        template = fields.language_template(2)
+        self.assertEqual(template, fields.MULTIPLE_LANGUAGE_TEMPLATE)
